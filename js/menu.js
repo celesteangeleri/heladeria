b38ab907e692d41654d07194fa912a0b388bf60b
Celeste Angeleri <angelericeleste@gmail.com>
2021-10-11 10:31:43 -0300

    ads

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnmenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnmenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+})
